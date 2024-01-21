ee189fc580735dfbf77797ec6be6b5798e12e349
Jillo Wario <willywario0@gmail.com>
2024-01-21 18:15:52 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,5 +1,77 @@
 /* Your Code Here */
 
+let createEmployeeRecord = function (array) {
+    return {
+      firstName: array[0],
+      familyName: array[1],
+      title: array[2],
+      payPerHour: array[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  };
+  
+  let createEmployeeRecords = function (employeeRowData) {
+    return employeeRowData.map(function (array) {
+      return createEmployeeRecord(array);
+    });
+  };
+  
+  let createTimeInEvent = function (dateStamp) {
+    let [date, hour] = dateStamp.split(' ');
+  
+    this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date,
+    });
+  
+    return this;
+  };
+  
+  let createTimeOutEvent = function (dateStamp) {
+    let [date, hour] = dateStamp.split(' ');
+  
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date,
+    });
+  
+    return this;
+  };
+  
+  let hoursWorkedOnDate = function (soughtDate) {
+    let inEvent = this.timeInEvents.find(function (e) {
+      return e.date === soughtDate;
+    });
+  
+    let outEvent = this.timeOutEvents.find(function (e) {
+      return e.date === soughtDate;
+    });
+  
+    return (outEvent.hour - inEvent.hour) / 100;
+  };
+  
+  let wagesEarnedOnDate = function (dateSought) {
+    let rawWage = hoursWorkedOnDate.call(this, dateSought) * this.payPerHour;
+    return parseFloat(rawWage.toString());
+  };
+  
+  let findEmployeeByFirstName = function (srcArray, firstName) {
+    return srcArray.find(function (rec) {
+      return rec.firstName === firstName;
+    });
+  };
+  
+  const calculatePayroll = function (arrayOfEmployeeRecords) {
+    return arrayOfEmployeeRecords.reduce(function (memo, rec) {
+      return memo + allWagesFor.call(rec);
+    }, 0);
+  };
+  
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
